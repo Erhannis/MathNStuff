69fa66c3986ae077643a0d6a315b5ecc4d6cbe01
Matthew Ewer <Eyeillus@gmail.com>
2013-05-09 18:47:17 -0700

    Added some R function operations.

--- src/mathnstuff/RToRFunction.java
+++ src/mathnstuff/RToRFunction.java
@@ -10,4 +10,40 @@ package mathnstuff;
  */
 public abstract class RToRFunction {
     public abstract double evaluate(double x);
+    
+    public RToRFunction plus(final RToRFunction b) {
+        return new RToRFunction() {
+            @Override
+            public double evaluate(double x) {
+                return evaluate(x) + b.evaluate(x);
+            }
+        };
+    }
+
+    public RToRFunction minus(final RToRFunction b) {
+        return new RToRFunction() {
+            @Override
+            public double evaluate(double x) {
+                return evaluate(x) - b.evaluate(x);
+            }
+        };
+    }
+
+    public RToRFunction times(final RToRFunction b) {
+        return new RToRFunction() {
+            @Override
+            public double evaluate(double x) {
+                return evaluate(x) * b.evaluate(x);
+            }
+        };
+    }
+
+    public RToRFunction div(final RToRFunction b) {
+        return new RToRFunction() {
+            @Override
+            public double evaluate(double x) {
+                return evaluate(x) / b.evaluate(x);
+            }
+        };
+    }
 }
