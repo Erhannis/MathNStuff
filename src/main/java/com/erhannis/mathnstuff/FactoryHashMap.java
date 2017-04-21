ae45342d3c5aa215d098350731c7af6b07cb8a3d
erhannis <Eyeillus@gmail.com>
2017-04-20 20:40:10 -0400

    Made FactoryHashMap

---
+++ src/main/java/com/erhannis/mathnstuff/FactoryHashMap.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.erhannis.mathnstuff;
+
+import com.erhannis.mathnstuff.utils.Factory;
+import java.util.HashMap;
+
+/**
+ * HashMap, but when you call "get" and it isn't in the map, a new one is created
+ * via the Factory and stored in the map, then returned.
+ * 
+ * Not thread safe.
+ * 
+ * @author MEwer
+ */
+public class FactoryHashMap<T, U> extends HashMap<T, U> {
+  private Factory<U> defaultConstructor;
+  
+  public FactoryHashMap(Factory<U> defaultConstructor) {
+    this.defaultConstructor = defaultConstructor;
+  }
+
+  @Override
+  public U get(Object key) {
+    if (containsKey(key)) {
+      return super.get(key);
+    } else {
+      U value = defaultConstructor.construct();
+      super.put((T)key, value);
+      return value;
+    }
+  }
+}
